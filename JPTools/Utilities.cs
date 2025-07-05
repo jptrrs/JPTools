98c58daf1313c32280035afa0b36cccaf66f299f
jptrrs <jaopedro@gmail.com>
2025-07-04 22:01:40 -0300

    Implemented fail safe method for retrieving def labels

---
+++ JPTools/Utilities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Verse;
+
+namespace JPTools
+{
+    public static class Utility
+    {
+        //Retrieves a Def's label while avoiding incomplete ones by falling back to the defName. It's a Function and not a Method so it can be used in LINQ queries. 
+        public static Func<Def, string> DefLabelFailSafe = (a) => a.label ?? a.defName ?? "Unlabeled";
+    }
+}
